8c9d004adecb205754d2601851c624f2d68e3841
yjkim <yjkim@sgu.co.kr>
2024-09-11 12:29:17 +0900

    강의 첫번째 앱 - 이미지 앱 완성

--- Image_app/app/src/main/java/com/kyj/imageapp/ImageInsideActivity.kt
+++ Image_app/app/src/main/java/com/kyj/imageapp/ImageInsideActivity.kt
@@ -1,6 +1,7 @@
 package com.kyj.imageapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +12,8 @@ class ImageInsideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_inside)
 
+        val imageInside = findViewById<ImageView>(R.id.image_inside)
+
+        imageInside.setImageResource(intent.getIntExtra("image", 0))
     }
 }
--- Image_app/app/src/main/java/com/kyj/imageapp/MainActivity.kt
+++ Image_app/app/src/main/java/com/kyj/imageapp/MainActivity.kt
@@ -41,61 +41,59 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        image1?.setOnClickListener(null)
+        image1?.setOnClickListener { onClick(it) }
+        image2?.setOnClickListener { onClick(it) }
+        image3?.setOnClickListener { onClick(it) }
+        image4?.setOnClickListener { onClick(it) }
+        image5?.setOnClickListener { onClick(it) }
+        image6?.setOnClickListener { onClick(it) }
+        image7?.setOnClickListener { onClick(it) }
+        image8?.setOnClickListener { onClick(it) }
+        image9?.setOnClickListener { onClick(it) }
     }
 
-    fun onClick(view: View): Unit {
-        when(view.id) {
+    fun onClick(view: View) {
+        val intent = Intent(this, ImageInsideActivity::class.java)
+
+        when (view.id) {
             R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+                intent.putExtra("image", R.drawable.image1)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image2 -> {
+                intent.putExtra("image", R.drawable.image2)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image3 -> {
+                intent.putExtra("image", R.drawable.image3)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image4 -> {
+                intent.putExtra("image", R.drawable.image4)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image5 -> {
+                intent.putExtra("image", R.drawable.image5)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image6 -> {
+                intent.putExtra("image", R.drawable.image6)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image7 -> {
+                intent.putExtra("image", R.drawable.image7)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image8 -> {
+                intent.putExtra("image", R.drawable.image8)
             }
 
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
-            }
-
-            R.id.image1 -> {
-                intent?.putExtra("image1", R.drawable.image1)
-                startActivity(intent)
+            R.id.image9 -> {
+                intent.putExtra("image", R.drawable.image9)
             }
         }
+
+        startActivity(intent)
     }
 
 }
